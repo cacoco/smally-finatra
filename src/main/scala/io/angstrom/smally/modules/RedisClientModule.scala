b9dc8ecdeb2085ec2e6ed5ac14a821ef9764f89d
Christopher Coco <cacoco@gmail.com>
2015-06-28 21:28:53 -0700

    Support auth.

--- src/main/scala/io/angstrom/smally/modules/RedisClientModule.scala
+++ src/main/scala/io/angstrom/smally/modules/RedisClientModule.scala
@@ -1,6 +1,7 @@
 package io.angstrom.smally.modules
 
 import com.google.inject.{Provides, Singleton}
+import com.twitter.finagle.redis.util.StringToChannelBuffer
 import com.twitter.finagle.redis.{Client, TransactionalClient}
 import com.twitter.inject.{Logging, TwitterModule}
 import io.angstrom.smally.modules.RedisClientModule._
@@ -18,20 +19,25 @@ class RedisClientModule
   @Singleton
   @Provides
   def providesRedisTransactionalClient(): Client = {
-    val configuration = parseRedisHostPortFromUrl()
-    TransactionalClient(s"${configuration.host}:${configuration.port}")
+    val configuration = parseRedisUrl
+    val client = TransactionalClient(s"${configuration.host}:${configuration.port}")
+    configuration.passwordOpt map { password => client.auth(StringToChannelBuffer(password)) }
+    client
   }
 
   /* Private */
 
-  private def parseRedisHostPortFromUrl(): RedisConfiguration = {
+  private def parseRedisUrl: RedisConfiguration = {
     val url = redisUrl()
-    info(s"Configured Redis URL: $url")
-    RedisConfiguration(
-      host = url.substring(RedisUrlScheme.length, url.lastIndexOf(":")),
-      port = java.lang.Integer.valueOf(url.substring(url.lastIndexOf(":") + 1))
-    )
+    val port = java.lang.Integer.valueOf(url.substring(url.lastIndexOf(":") + 1))
+
+    val potentialHost = url.substring(RedisUrlScheme.length, url.lastIndexOf(":"))
+    val (host, passwordOpt) = if (potentialHost.contains("@")) {
+      (potentialHost.substring(potentialHost.indexOf("@") + 1),
+        Some(potentialHost.substring(0, potentialHost.indexOf("@"))))
+    } else (potentialHost, None)
+    RedisConfiguration(host, port, passwordOpt)
   }
 }
 
-case class RedisConfiguration(host: String, port: Int)
+case class RedisConfiguration(host: String, port: Int, passwordOpt: Option[String])
